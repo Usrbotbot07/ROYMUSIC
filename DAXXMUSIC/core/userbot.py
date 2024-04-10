f6a778b92e0e9259ad284c45e50c583817975e2c
Usrbotbot07 <160359608+Usrbotbot07@users.noreply.github.com>
2024-04-10 22:54:15 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("The_friendz")
+                await self.two.join_chat("CRAZY_ADDA")
                 
-                await self.one.join_chat("RoY_editx")
+                await self.one.join_chat("CRAZY_ADDA")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("The_friendz")
+                await self.three.join_chat("CRAZY_ADDA")
                 
-                await self.one.join_chat("RoY_editx")
+                await self.one.join_chat("CRAZY_ADDA")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("The_friendz")
+                await self.four.join_chat("CRAZY_ADDAz")
                 
-                await self.one.join_chat("roy_editx")
+                await self.one.join_chat("CRAZY_ADDA")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("The_friendz")
+                await self.five.join_chat("CRAZY_ADDA")
                 
-                await self.one.join_chat("roy_editx")
+                await .self.one.join_chat("CRAZY_ADDA")
             except:
                 pass
             assistants.append(5)
